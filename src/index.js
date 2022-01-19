009a64cb12bbba3551c8eee7649fdbe4c1327160
AlvaroPerezRey <aperez.alumno@iesrafaelalberti.es>
2022-01-19 15:47:20 +0100

    Index preparado para comenzar proyecto react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const element = (
+  <div>
+    <Title />
+    <Welcome name={user.name}></Welcome>
+    <Definitions></Definitions>
+    <Comment author={user}></Comment>
+    <HowItWorks></HowItWorks>
+    <hr/>
+    <Footnote id="1" description="Los componentes son reutilizables, como ves en estas notas que estamos escribiendo."></Footnote>
+    <Footnote id="2" description="Los componentes que tú crees siempre deben empezar con MAYÚSCULA, porque si empiezan por minúscula, React lo tomará como una etiqueta DOM."></Footnote>
+    <Footnote id="3" description="Los componentes, pueden formar parte de otros componentes, mira el componente Comment"></Footnote>
+    <Footnote id="4" description="Las funciones de un componente nunca deben modificar las entradas."></Footnote>
+    <Footnote id="5" description="Podemos llamar a multiples componentes dentro de un componente."></Footnote>
+  </div>
+);
+
+const App = () => (
+  <div className='app'>
+      <h1>CRUD</h1>
+      <fieldset className='crud-input'>
+          <legend className='crud-input__label'>Nuevo elemento </legend>
+          <input className='crud-input__input' placeholder='Introduzca el elemento'/>
+      </fieldset>
+      <ul className='crud-list'>
+          {elems.map(({text,done}, i) => (
+            <li className={`crud-list__item ${done? 'crud-list__item--done':''}`}>
+                <label htmlFor={`e${i}`}>
+                  <input id={`e${i}`} type='checkbox' checked={done}/>
+                  {text}
+                </label>
+            </li> 
+          ))}
+      </ul>
+      <button className='crud-clear' type='button'>Borrar hechos</button>
+  </div>
+)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  element,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/* contenedor 
+    crear
+    leer
+        lista
+        item
+              */
