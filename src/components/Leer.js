0852fd2a8ff5b068b6eac67f63dbc0e218a10ca7
AlvaroPerezRey <aperez.alumno@iesrafaelalberti.es>
2022-01-27 19:27:03 +0100

    Borrar un elemento concreto

--- src/components/Leer.js
+++ src/components/Leer.js
@@ -4,12 +4,21 @@ import Item from './Item'
 const Leer = ({elems, onElemsChange}) => (
     <ul className="crud-list">
         {elems.map(({text, done}, i) => (
-            <Item text={text} done={done} i={i} 
-                onDoneChange={value => { const updateElems = [ ...elems ];
-                    updateElems[i].done = value;
-                    onElemsChange(updateElems);
-                }
-            }/>
+            <li key={i} className={`crud-list__item ${done? 'crud-list__item--done':''}`}>
+                <Item text={text} done={done} i={i} 
+                    onDoneChange={value => { const updateElems = [ ...elems ];
+                        updateElems[i].done = value;
+                        onElemsChange(updateElems);
+                    }
+                }/>
+                <button onClick={() => {
+                    const acciones = [...elems]
+                    acciones.splice(i,1)
+                    onElemsChange(acciones);
+                    }
+                }>Borrar elementos </button>
+
+            </li>
         ))}
     </ul>
 );
